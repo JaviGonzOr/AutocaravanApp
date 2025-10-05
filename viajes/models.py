0a6af31899a0d4124fde76263a9e535eb96d2c35
GonzOr <helice.uno@gmail.com>
2025-10-05 20:30:54 +0200

    Primer commit - Proyecto Django 14

--- viajes/models.py
+++ viajes/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
+
 class Viaje(models.Model):
     usuario = models.ForeignKey(User, on_delete=models.CASCADE)
     titulo = models.CharField(max_length=200)
